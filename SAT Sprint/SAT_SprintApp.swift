7f782911374e38b94f1a8822f7eada698f1520f2
Izbassar <izok2004@gmail.com>
2026-01-17 23:16:38 +0500

    Initial Commit

---
+++ SAT Sprint/SAT_SprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAT_SprintApp.swift
+//  SAT Sprint
+//
+//  Created by Izbassar Orynbassar on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SAT_SprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
